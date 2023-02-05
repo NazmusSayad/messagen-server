06f6fc3329be0e337b7309c5db9cb95765142a48
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2023-02-05 00:37:21 +0000

    idk

--- src/controller/message/messageSocket.ts
+++ src/controller/message/messageSocket.ts
@@ -1,9 +1,18 @@
+import Message from '../../model/Message'
 import { SocketController } from '../../utils/socket'
 
-export const getMessage: SocketController = async (info) => {}
+export const getMessage: SocketController = async (info) => {
+  const messages = await Message
+}
 
-export const createMessage: SocketController = async (info) => {}
+export const createMessage: SocketController = async (info) => {
+  const message = await Message
+}
 
-export const updateMessage: SocketController = async (info) => {}
+export const updateMessage: SocketController = async (info) => {
+  const message = await Message
+}
 
-export const deleteMessage: SocketController = async (info) => {}
+export const deleteMessage: SocketController = async (info) => {
+  const message = await Message
+}
