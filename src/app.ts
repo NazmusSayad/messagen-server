776c44c2b1da1899f4eb796f82fa9a5b67dfb010
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2023-02-21 07:55:26 +0000

    use all origin

--- src/app.ts
+++ src/app.ts
@@ -13,11 +13,7 @@ const app = express()
 app.use(
   cors({
     credentials: true,
-    origin: Object.entries(process.env)
-      .filter(([name]) => {
-        return name.startsWith('CLIENT')
-      })
-      .map(([, value]) => value),
+    origin: /.*/,
   })
 )
 app.use(helmet())
