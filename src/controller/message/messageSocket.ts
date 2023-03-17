0a50aaaa655f0528c826f3f01b6e164ce24f6a45
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2023-03-17 14:09:07 +0000

    message fixed

--- src/controller/message/messageSocket.ts
+++ src/controller/message/messageSocket.ts
@@ -1,3 +1,4 @@
+import { checkType } from 'express-master'
 import Contact from '../../model/Contact'
 import Message from '../../model/Message'
 import { SocketController } from '../../utils/socket'
@@ -7,25 +8,18 @@ export const getMessage: SocketController = async (info) => {
 }
 
 export const createMessage: SocketController = async (info) => {
-  const { friend, group, text, images } = info.data
-  if ((friend && group) || (!text && !images)) {
-    throw new ReqError('heljasdkf kalsjdfkas dfkjasf')
+  const { to, text, images } = info.data
+  checkType.string({ to })
+  if (!text && !images) {
+    throw new ReqError('Message must not be empty')
   }
 
-  let col
-  if (friend) {
-    const isOk = await Contact.findById(friend)
-    if (isOk) col = { friend }
-  } else if (group) {
-    const isOk = await Contact.findById(group)
-    if (isOk) col = { group }
-  } else {
-    throw new ReqError('NOthing found!')
-  }
+  const isExists = await Contact.findById(to)
+  if (!isExists) throw new ReqError('Contact does not exists')
 
   const message = await Message.create({
-    ...col,
     from: info.user,
+    to,
     text,
     images,
   })
